46428b092339318caa538ce63e1d8fd6a93d9d2c
Daniel Brunat <dbrunat@grupomercel.com>
2022-10-26 11:01:40 -0300

    first commit

--- pages/home/home.tsx
+++ pages/home/home.tsx
@@ -1,40 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { Fade } from "react-awesome-reveal"
-import {
-  VFooter,
-  VMainHeader,
-  VMenuBar,
-  VToolsTech
-} from '../../utils'
-import {
-  chevronDown,
-  profilePicture
-} from '../../assets'
-import styles from './home.module.scss'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Fade } from "react-awesome-reveal";
+import { VFooter, VMainHeader, VMenuBar, VToolsTech } from "../../utils";
+import { chevronDown, profilePicture } from "../../assets";
+import styles from "./home.module.scss";
 
 export default function VHome() {
   return (
     <div className={styles.homeContainer}>
       <Head>
-        <title>Karolina Hudziec</title>
-        <meta name="description" content="This is my portfolio page. The purpose of this page is to introduce my person, my coding style, and show off a little bit of my web development skills." />
-        <meta name="author" content="Karolina Hudziec"/>
-        <meta name="keywords" content="Karolina, Hudziec, Portfolio, Frontend"/>
-        <meta property="og:image" content="https://www.khudziec.com/portfolioPage.png" key="ogimage"/>
-        <link rel="shortcut icon" href="/favicon.ico?" type="image/x-icon"/>
+        <title>Daniel Brunat</title>
+        <meta
+          name="description"
+          content="This is my portfolio page. The purpose of this page is to introduce my person, my coding style, and show off a little bit of my web development skills."
+        />
+        <meta name="author" content="Daniel Brunat" />
+        <meta name="keywords" content="Daniel, Brunat, Portfolio, Frontend" />
+        <meta
+          property="og:image"
+          content="https://www.khudziec.com/portfolioPage.png"
+          key="ogimage"
+        />
+        <link rel="shortcut icon" href="/favicon.ico?" type="image/x-icon" />
       </Head>
-      <VMenuBar activeTab="Home"/>
+      <VMenuBar activeTab="Home" />
       <div className={styles.content}>
         <div className={styles.intro}>
           <div className={styles.introduction}>
-            <VMainHeader/>
+            <VMainHeader />
             <div className={styles.profilePicture}>
               <Image
-                alt="Profile picture of Karolina Hudziec."
-                src={profilePicture}/>
+                alt="Profile picture of Daniel Brunat."
+                src={profilePicture}
+              />
             </div>
           </div>
           <div className={styles.swipeDownIcon}>
@@ -42,14 +42,23 @@ export default function VHome() {
               alt="Chevron down icon."
               src={chevronDown}
               width="40px"
-              height="15px"/>
+              height="15px"
+            />
           </div>
         </div>
         <div className={styles.workHistory}>
           <div className={styles.linkBox}>
             <Fade>
-              <h1 className={styles.linkBoxTitle}>Work<br/>History</h1>
-              <h2 className={styles.linkBoxTextContent}>Here you can find out more about the companies I've collaborated with, projects I've been part of and the technologies I use every day.</h2>
+              <h1 className={styles.linkBoxTitle}>
+                Work
+                <br />
+                History
+              </h1>
+              <h2 className={styles.linkBoxTextContent}>
+                Here you can find out more about the companies I've collaborated
+                with, projects I've been part of and the technologies I use
+                every day.
+              </h2>
               <Link href={`/work/work`}>
                 <a className={styles.linkBoxButton}>
                   Check out my work history
@@ -57,30 +66,33 @@ export default function VHome() {
               </Link>
             </Fade>
           </div>
-          <div className={styles.workHistoryBackground}/>
+          <div className={styles.workHistoryBackground} />
         </div>
         <div className={styles.technologiesAndTools}>
           <Fade>
             <h1>Technologies & Tools I work with</h1>
-            <VToolsTech/>
+            <VToolsTech />
           </Fade>
         </div>
         <div className={styles.personalProjects}>
-            <div className={styles.linkBox}>
-              <Fade>
-                <h1 className={styles.linkBoxTitle}>About me</h1>
-                <h2 className={styles.linkBoxTextContent}>Here you can find out more about me, my hobbies and personal project I'm currently working on.</h2>
-                <Link href={`/about/about`}>
-                  <a className={styles.linkBoxButton}>
-                    Get to know more about me
-                  </a>
-                </Link>
-              </Fade>
-            </div>
-          <div className={styles.personalProjectsBackground}/>
+          <div className={styles.linkBox}>
+            <Fade>
+              <h1 className={styles.linkBoxTitle}>About me</h1>
+              <h2 className={styles.linkBoxTextContent}>
+                Here you can find out more about me, my hobbies and personal
+                project I'm currently working on.
+              </h2>
+              <Link href={`/about/about`}>
+                <a className={styles.linkBoxButton}>
+                  Get to know more about me
+                </a>
+              </Link>
+            </Fade>
+          </div>
+          <div className={styles.personalProjectsBackground} />
         </div>
       </div>
-      <VFooter/>
+      <VFooter />
     </div>
-  )
+  );
 }
